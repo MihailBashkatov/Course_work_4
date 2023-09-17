0f00663627b4cf7b2d14cb4ac4301c4269673494
mihail <mihail.bashkatov@gmail.com>
2023-09-17 19:55:38 +0200

    Docstrings are added for main.py

--- main.py
+++ main.py
@@ -4,16 +4,28 @@ from utils.utils import get_started, get_filter, get_comparison, get_sorted
 
 
 def user_interaction():
+    """Intersection with user.
+    Covering set of question and depending on what user is answering, programme define next steps"""
+
     while True:
+
+        # Defining which platform will be used: Headhunter or Superjob
         chosen_platform = get_started()
+
+        # User inserting word, which will be used for initializing object of class Vacancy
         choose_word = input('Insert word: ').strip().lower()
 
         vacancy = Vacancy(choose_word)
-        if chosen_platform == platforms[0].lower().strip():
 
+        # Initializing search in Headhunter
+        if chosen_platform == platforms[0].lower().strip():
             vacancy.initialize_vacancies_headhunter()
+
+        # Initializing search in Superjob
         elif chosen_platform == platforms[1].lower().strip():
             vacancy.initialize_vacancies_superjob()
+
+        # Defining where to search inserted word
         while True:
             print('\nInsert number which area you want to search')
             filter_part = input('Everywhere: [1]\nEmployee name: [2]\n'
@@ -21,18 +33,30 @@ def user_interaction():
                                 'Vacancy description: [4]\n'
                                 'Check Favorites: [5]\n'
                                 'Compare vacancies: [6]\nQuit: [0]\n')
+
+            # Showing vacancies under chosen mode
             response = get_filter(filter_part, vacancy)
+
+            # If user chose something out of proposed options, programme comes back to options
             if response == 1:
                 continue
+
+            # If user wants to compare the lowest salary level for two vacancies in Favorite
             elif response == 2:
                 try:
+
+                    # Vacancies in Favorite
                     vacancy_list = vacancy.vacancies_favorite
                     if len(vacancy_list) < 2:
                         print('\nFavorites does have less, than 2 vacancies')
                         continue
                     while True:
+
+                        # User enters id vacancy 1
                         vacancy_1 = int(input('Make sure, that Favorites does have more than 1 vacancy'
                                               'Insert vacancy_1 ID\n If want to stop comparison, insert [3]\n'))
+
+                        # User enters id vacancy 2
                         if vacancy_1 == 3:
                             break
                         vacancy_2 = int(input('Make sure, that Favorites does have more than 1 vacancy'
@@ -40,20 +64,27 @@ def user_interaction():
                         if vacancy_2 == 3:
                             break
 
+                        # Actual comparison
                         comparison = get_comparison(vacancy_list, vacancy_1, vacancy_2)
                         if type(comparison) == str:
                             print(comparison)
                             continue
+
+                # if user inserted not digits
                 except ValueError:
                     print('\nID shall be digits, not letters')
             else:
                 break
 
+        # Defining if sorting is needed
         is_sort_data = input('Do you want to sort data? If yes, insert [1]. If no, insert [any button]')
         if is_sort_data == '1':
+
+            # Defining sorting mode: ascending orf descending
             sort_mode = input('\nWhich order you want to sort? If Ascending, insert [1]. If Descending, insert [2]')
             get_sorted(filter_part, sort_mode, vacancy)
 
+        # Defining if saving to Favorites is needed
         save_mode = input('\nWould you like to save data? If yes, insert [1]. '
                           'If no, start again [any button] or quite [0]\n')
 
